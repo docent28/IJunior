9616702a78a62a47c4ee9134a73a6f6b09746ca7
Docent28 <eaa67@mail.ru>
2020-12-15 20:49:50 +0300

    Начинаем задачу 33 Построено игровое поле

---
+++ EVL_HomeWork_33/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVL_HomeWork_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayingField playingField = new PlayingField();
+            playingField.CreatingField();
+            Console.CursorVisible = false;
+
+            Console.SetCursorPosition(2, 1);
+            Console.Write("R");
+            Console.SetCursorPosition(6, 1);
+            Console.Write("O");
+            Console.SetCursorPosition(6, 5);
+            Console.Write("T");
+            Console.SetCursorPosition(6, 3);
+            Console.Write("D");
+            Console.SetCursorPosition(34, 19);
+            Console.Write("H");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Fighter
+    {
+        private string _name;
+        private int _health;
+        private int _damage;
+        private int _armor;
+
+        public Fighter(string name, int health, int damage, int armor)
+        {
+            _name = name;
+            _health = health;
+            _damage = damage;
+            _armor = armor;
+        }
+    }
+
+    class PlayingField
+    {
+        public void CreatingField()
+        {
+            int fieldSize = 10;
+
+            for (int i = 0; i < fieldSize; i++)
+            {
+                Console.WriteLine("|---|---|---|---|---|---|---|---|---|---|");
+                Console.WriteLine("|   |   |   |   |   |   |   |   |   |   |");
+            }
+            Console.WriteLine("|---|---|---|---|---|---|---|---|---|---|");
+        }
+    }
+}
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,53 +7,10 @@ namespace YNI_Task_03
     {
         static void Main(string[] args)
         {
-            List<Item> items = new List<Item>();
-            string nameItem;
-            int quantityItem;
-            double priceItem;
-            double sum = 0;
-
-
-
-            nameItem = "тов1";
-            quantityItem = Convert.ToInt32(Console.ReadLine());
-            priceItem = (double)Convert.ToInt32("55") / (double)(quantityItem);
-            Item item = new Item(nameItem, quantityItem, priceItem);
-            items.Add(item);
-
-            nameItem = "тов2";
-            quantityItem = Convert.ToInt32(Console.ReadLine());
-            priceItem = (double)Convert.ToInt32("75") / (double)(quantityItem);
-            Item item1 = new Item(nameItem, quantityItem, priceItem);
-            items.Add(item1);
-
-            nameItem = "тов3";
-            quantityItem = Convert.ToInt32(Console.ReadLine());
-            priceItem = (double)Convert.ToInt32("91") / (double)Convert.ToInt32("23");
-            Item item2 = new Item(nameItem, quantityItem, priceItem);
-            items.Add(item2);
-
-            foreach (var var_item in items)
-            {
-                sum += var_item.Price * var_item.Quantity;
-            }
-
-            Console.WriteLine(Math.Round(sum, 2));
+ 
+            Console.WriteLine("555");
+            Console.WriteLine("♫");
 
         }
     }
-
-    class Item
-    {
-        public string Name { get; }
-        public int Quantity { get; set; }
-        public double Price { get; set; }
-
-        public Item(string name, int quantity, double price)
-        {
-            Name = name;
-            Quantity = quantity;
-            Price = price;
-        }
-    }
-}
+ }
